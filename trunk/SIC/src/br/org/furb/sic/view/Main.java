264f4b73bf69e520580ebf507cc4a9d6ed7c0b6a
reiinoldo@gmail.com <reiinoldo@gmail.com@4ed0bf91-868d-3837-de7e-6fbffad66633>
2014-04-25 03:57:03 +0000

    Implementado busca da timeline do usuário

---
+++ trunk/SIC/src/br/org/furb/sic/view/Main.java
@@ -0,0 +1,15 @@
+package br.org.furb.sic.view;
+
+import br.org.furb.sic.controller.TwitterController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TwitterController tc = new TwitterController();
+		tc.buscaPalavraChave("Furb");
+
+	}
+
+}
