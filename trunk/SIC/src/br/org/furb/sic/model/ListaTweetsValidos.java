b43e2c3f2d59bd93feeea5aa3990d21fac7bae9a
reiinoldo@gmail.com <reiinoldo@gmail.com@4ed0bf91-868d-3837-de7e-6fbffad66633>
2014-04-26 05:28:09 +0000

    Tweets váidos

--- trunk/SIC/src/br/org/furb/sic/model/ListaTweetsValidos.java
+++ trunk/SIC/src/br/org/furb/sic/model/ListaTweetsValidos.java
@@ -1,15 +1,36 @@
 package br.org.furb.sic.model;
 
+import twitter4j.Status;
+
 /** 
  * @category Consumidor
  */
 public class ListaTweetsValidos {
 	
-	public ListaTweetsValidos(){
-		
+	private ListaTweets listaTweets;
+	
+	public ListaTweetsValidos(ListaTweets lista){
+		listaTweets = lista;		
 	}
+	
 	// Método para ser acordado para consumir 
-	public void validaTweet(){
+	public void validaTweet(String palavraChave){
+		// Lock? 
+		Status status = listaTweets.removeTweet();
+		// UnLock?
+		
+		// Faz algo fodastico \/
+		// Dava de fazer uma viagem aqui, criava varias threads mexendo no mesmo dado, verificando a mensagem se é valida
+		System.out.println("Validando tweet: " + status.getText());		
+		
+		// Ok é valido
+		if (status.getText().contains(palavraChave)){
+			// Faz o procedimento para buscar a timeline do usuário;
+			// Monta perfil do usuário
+			// status.getUser().getProfileImageURL();
+			// status.getUser().getLocation();
+			// status.getUser().getDescription();
+		}		
 		
 	}
 
