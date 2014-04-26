c0929594a4d0308ea0b6df6c24dc9978b2724f07
maicon.gerardi@gmail.com <maicon.gerardi@gmail.com@4ed0bf91-868d-3837-de7e-6fbffad66633>
2014-04-26 14:49:12 +0000

    Adicionar algumas lógicas para a lista de inserção de tweets.

--- trunk/SIC/src/br/org/furb/sic/model/ListaTweets.java
+++ trunk/SIC/src/br/org/furb/sic/model/ListaTweets.java
@@ -2,45 +2,71 @@ package br.org.furb.sic.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 import twitter4j.Status;
+import br.org.furb.sic.view.Main;
 
-/** 
+/**
  * @category Produtor
  */
-public class ListaTweets {	
+public class ListaTweets {
+	private final int LIMITE_LISTA = 100;
+	private final int LIMITE_THREAD = 20;
 	
 	private List<Status> lista;
-	private final int LIMITE_LISTA = 20;
-	
-	public ListaTweets(){
+	private Semaphore semaforo;
+
+	public ListaTweets() {
 		lista = new ArrayList<Status>();
+		semaforo = new Semaphore(LIMITE_THREAD);
+		new Thread(){
+			@Override
+			public void run() {
+				while(true){
+					try {
+						semaforo.acquire();
+					
+						/**
+						 * Iniciar uma thread se existir recurso para isso.
+						 */
+						TratarTweetThread thread = new TratarTweetThread(lista.remove(0), semaforo);
+						thread.start();
+						
+						// TODO falta finalizar Thread após acabar os tweets
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
 	}
-	
-	public List<Status> getLista(){
+
+	public List<Status> getLista() {
 		return lista;
 	}
-	
-	public synchronized void insereTweet(Status tweet) throws InterruptedException{
-		
-		while(lista.size() > LIMITE_LISTA)
-			wait();	
-		
+
+	public synchronized void insereTweet(Status tweet)
+			throws InterruptedException {
+		while (lista.size() > LIMITE_LISTA)
+			wait();
+
 		lista.add(tweet);
-		
+		Main.print(getClass(), "tamanho(" + lista.size()
+				+ ") adicionou tweet na ListaTweet: " + tweet.toString());
+
+
 		notifyAll();
 	}
-	
+
 	// Precisa dar um Lock para remover?
-	public Status removeTweet(){
-		
+	public Status removeTweet() {
+
 		if (!lista.isEmpty())
 			return lista.remove(0);
-		
-		return null;		
-		
+
+		return null;
+
 	}
-	
-	 
 
 }
--- trunk/SIC/src/br/org/furb/sic/view/Main.java
+++ trunk/SIC/src/br/org/furb/sic/view/Main.java
@@ -3,14 +3,17 @@ package br.org.furb.sic.view;
 import br.org.furb.sic.controller.TwitterController;
 
 public class Main {
+	private static final boolean DEBUG = true;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		TwitterController tc = new TwitterController();		
+		TwitterController tc = new TwitterController();
 		tc.buscaPalavraChave("Furb");
-		
+	}
 
+	public static void print(Class<?> clazz, String msg) {
+		if (DEBUG) {
+			System.out.println("[" + clazz.getName() + "] " + msg);
+		}
 	}
 
 }
