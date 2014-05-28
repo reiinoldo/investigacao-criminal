683a8b53fe4791101abcf55004a35a05c08f6f16
andrevbampi@gmail.com <andrevbampi@gmail.com@4ed0bf91-868d-3837-de7e-6fbffad66633>
2014-05-28 22:40:36 +0000

    - Buscar na lista de Facebook no OMPMostrarTweet

--- trunk/SIC/src/br/org/furb/sic/controller/omp/OmpMostrarTweets_jomp.java
+++ trunk/SIC/src/br/org/furb/sic/controller/omp/OmpMostrarTweets_jomp.java
@@ -107,6 +107,8 @@ private class __omp_Class0 extends jomp.runtime.BusyTask {
 				exibirNaTela += "Nome: " + tweet.getUser().getName() + "\n";
 				exibirNaTela += sdf.format(tweet.getCreatedAt()) + " - " + tweet.getText() + "\n";
 
+				// pq o hash map?
+				// n\u00e3o deveria usar o m\u00e9todo cincoUltimosTweetsUsuario()?
 				exibirNaTela += listaCincoUltimosTweets.get(tweet.getUser().getId());
 				
 				/*List cincoUltimosTweetsUsuario = null;
@@ -125,7 +127,7 @@ private class __omp_Class0 extends jomp.runtime.BusyTask {
 					exibirNaTela += "Nenhum tweet recente.\n";
 				}*/
 
-				exibirNaTela += "\u00c0 fazer - Perfil no Facebook\n";
+				exibirNaTela += listaPerfisFacebook.get(tweet.getUser().getId());
 				
 				System.out.println(exibirNaTela);
 			}
